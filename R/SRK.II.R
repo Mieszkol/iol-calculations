5e8f0692cfd2ffb382f71a363077958f1aaf2bc3
Mieszko Lachota <61295680+Mieszkol@users.noreply.github.com>
2023-08-20 16:56:35 +0200

    srkii
    i

--- R/SRK.II.R
+++ R/SRK.II.R
@@ -12,19 +12,20 @@
 #' @family Power
 SRK.II.Power <- function(L, K, A, ELP) {
   args <- list(L = L, K = K)
-  if (missing(A) || !unique(is.finite(A))) {
-    print(str(match.call()))
+  if (is.null(A) || all(!is.finite(A))) {
     A <- (ELP + 63.896) / 0.58357
     args$ELP <- ELP
     warning("Using Holladay approximation for SRK II's A from ELP constant: ", A)
   } else {
     args$A <- A
   }
+  
   A2 <- A
-  if (L < 20) A2 <- A + 3
-  if (L >= 20 & L < 21) A2 <- A + 2
-  if (L >= 21 & L < 22) A2 <- A + 1
-  if (L >= 24.5) A2 <- A - 0.5
+  A2[L < 20] <- A[L < 20] + 3
+  A2[L >= 20 & L < 21] <- A[L >= 20 & L < 21] + 2
+  A2[L >= 21 & L < 22] <- A[L >= 21 & L < 22] + 1
+  A2[L >= 24.5] <- A[L >= 24.5] - 0.5
+  
   P <- A2 - 2.5 * L - 0.9 * K
   attr(P, 'parameters') <- args
   return(P)
